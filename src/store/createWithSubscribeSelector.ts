3793522678c530b1cbace42c1814d4c8b00fd040
RealPeha <real.peha@gmail.com>
2022-05-15 12:01:42 +0300

    feat: createWithSubscribeSelector

---
+++ src/store/createWithSubscribeSelector.ts
@@ -0,0 +1,11 @@
+import { subscribeWithSelector } from 'zustand/middleware'
+import create, { Mutate, GetState, SetState, StoreApi, StateCreator, State } from 'zustand'
+
+export const createWithSubscribeSelector = <TState extends State>(createState: StateCreator<TState>) => {
+  return create<
+    TState,
+    SetState<TState>,
+    GetState<TState>,
+    Mutate<StoreApi<TState>, [['zustand/subscribeWithSelector', never]]>
+  >(subscribeWithSelector(createState))
+}
--- src/store/wallet/index.ts
+++ src/store/wallet/index.ts
@@ -1,11 +1,10 @@
 import { Nullable } from 'tsdef'
-import { subscribeWithSelector } from 'zustand/middleware'
-import create, { Mutate, GetState, SetState, StoreApi } from 'zustand'
 import { Action } from '..'
 import { deriveMnemonicAddress } from './deriveMnemonicAddress'
 import { generateWallet } from './generateWallet'
 import { importWallet } from './importWallet'
 import { useBlockchainStore } from '../blockchain'
+import { createWithSubscribeSelector } from '../createWithSubscribeSelector'
 
 export interface WalletStore {
   pathId: number
@@ -17,19 +16,13 @@ export interface WalletStore {
 
 export type WalletAction = Action<WalletStore>
 
-export const useWalletStore = create<
-  WalletStore,
-  SetState<WalletStore>,
-  GetState<WalletStore>,
-  Mutate<StoreApi<WalletStore>, [['zustand/subscribeWithSelector', never]]>
->(
-  subscribeWithSelector((set, get) => ({
-    pathId: 0,
-    phrase: null,
-    generateWallet: generateWallet(set, get),
-    importWallet: importWallet(set, get),
-    deriveMnemonicAddress: deriveMnemonicAddress(set, get),
-  })),
+export const useWalletStore = createWithSubscribeSelector<WalletStore>((set, get) => ({
+  pathId: 0,
+  phrase: null,
+  generateWallet: generateWallet(set, get),
+  importWallet: importWallet(set, get),
+  deriveMnemonicAddress: deriveMnemonicAddress(set, get),
+}),
 )
 
 useWalletStore.subscribe(
