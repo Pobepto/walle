5fda9c72405860459ee6e0b96936aa7b5bbd2697
Robert Sayakhov <roobertdantes@gmail.com>
2022-05-18 18:55:58 +0300

    :recycle:  Change fs promises

--- src/utils/fs.ts
+++ src/utils/fs.ts
@@ -1,18 +1,14 @@
 import fs from 'fs'
-import util from 'util'
-
-const writeFile = util.promisify(fs.writeFile)
-const readFile = util.promisify(fs.readFile)
 
 const USER_DATA = 'walle.data'
 
 export const save = async (data: string) => {
   // TODO: Add try catch later
-  await writeFile(USER_DATA, data)
+  await fs.promises.writeFile(USER_DATA, data)
 }
 
 export const load = async () => {
   // TODO: Add try catch later
-  const data = await readFile(USER_DATA, 'utf8')
+  const data = await fs.promises.readFile(USER_DATA, 'utf8')
   return JSON.parse(data)
 }
