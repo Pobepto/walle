3bb2a65ec54a74de5880b5ea2bf5e5fae7ab67da
Robert Sayakhov <roobertdantes@gmail.com>
2022-06-19 17:30:15 +0300

    :sparkles: Hook for saving settings

--- src/index.tsx
+++ src/index.tsx
@@ -2,9 +2,6 @@ import React from 'react'
 import { render } from 'ink'
 import { App } from './App'
 import { Router, ROUTE } from './routes'
-import singleton from './utils/singleton'
-
-const b = singleton
 
 render(
   <Router defaultPath={ROUTE.WELCOME}>
