b3808348e4e404a07b6899ab66fed147c14b6edb
RealPeha <real.peha@gmail.com>
2022-07-17 21:12:45 +0300

    :art: improve structure

---
+++ src/hooks/useForm/rules.ts
@@ -0,0 +1,37 @@
+import { getAddress } from '@ethersproject/address'
+
+export const length =
+  (min: number, max = Infinity) =>
+  (value: string) => {
+    if (value.length < min) {
+      return `Must be at least ${min} characters`
+    }
+
+    if (value.length > max) {
+      return `Must be at most ${max} characters`
+    }
+  }
+
+export const isNumber = () => (value: string) => {
+  return Number.isInteger(parseFloat(value)) ? undefined : 'Must be a number'
+}
+
+export const numberInRange = (min: number, max: number) => (value: string) => {
+  const number = Number(value)
+
+  if (number < min) {
+    return `Must be bigger than ${min}`
+  }
+
+  if (number > max) {
+    return `Must be lest than ${max}`
+  }
+}
+
+export const isAddress = () => (value: string) => {
+  try {
+    getAddress(value)
+  } catch (error) {
+    return `Address is invalid`
+  }
+}
--- src/hooks/useForm/useForm.ts
+++ src/hooks/useForm/useForm.ts
@@ -1,4 +1,3 @@
-import { getAddress } from '@ethersproject/address'
 import { isDefined } from '@src/utils'
 import { useMemo, useState } from 'react'
 
@@ -113,39 +112,3 @@ export const combine =
       return (err || rule(value, data)) as string
     }, '')
   }
-
-export const length =
-  (min: number, max = Infinity) =>
-  (value: string) => {
-    if (value.length < min) {
-      return `Must be at least ${min} characters`
-    }
-
-    if (value.length > max) {
-      return `Must be at most ${max} characters`
-    }
-  }
-
-export const isNumber = () => (value: string) => {
-  return Number.isInteger(parseFloat(value)) ? undefined : 'Must be a number'
-}
-
-export const numberInRange = (min: number, max: number) => (value: string) => {
-  const number = Number(value)
-
-  if (number < min) {
-    return `Must be bigger than ${min}`
-  }
-
-  if (number > max) {
-    return `Must be lest than ${max}`
-  }
-}
-
-export const isAddress = () => (value: string) => {
-  try {
-    getAddress(value)
-  } catch (error) {
-    return `Address is invalid`
-  }
-}
