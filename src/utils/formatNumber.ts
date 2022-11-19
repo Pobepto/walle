7b1c36cf34ced4be4c1c8665fad73f9ead5f50b1
Robert Sayakhov <roobertdantes@gmail.com>
2022-11-19 22:46:43 +0300

    :bug: Fix issue with empty balance

--- src/utils/formatNumber.ts
+++ src/utils/formatNumber.ts
@@ -4,6 +4,10 @@ import { formatUnits, commify } from '@ethersproject/units'
 export const formatNumber = (v: string, decimals = 18, visibleDecimals = 5) => {
   const bv = BigNumber.from(v)
 
+  if (bv.eq(BigNumber.from(0))) {
+    return '0'
+  }
+
   if (bv.lt(BigNumber.from(10).pow(6))) {
     return '< 0.000001'
   }
